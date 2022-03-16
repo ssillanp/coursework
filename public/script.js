7950dd190455df2a2005c1122c39367ccaa52e04
sami <sami.sillanpaa@iki.fi>
2022-03-16 22:35:55 +0200

    radItem removing functionality added

--- public/script.js
+++ public/script.js
@@ -1,48 +1,49 @@
 const headerText = document.getElementById("header-text")
 const buttons = document.querySelectorAll("button")
 const result = document.getElementById("result")
+const inputItem = document.getElementById("input-item")
+const inputText = document.getElementById("input-text")
+const inputCheck = document.getElementById("input-check")
 const listItems = []
 
-start()
+inputCheck.disabled = true
+
 
 buttons.forEach((button)=> {
     button.addEventListener("click", (e)=>{
-        console.log(e.currentTarget.id)
         if(e.currentTarget.id === "lists-btn"){
             selectList()
         } else if (e.currentTarget.id === "delete-itm-btn") {
-            deleteItem()
+            deleteItems()
         } else if (e.currentTarget.id === "undo--btn") {
             undo()
         }
     })
 })
 
-listItems[0].addEventListener("keyup", (e) => {
-    if (e.key === "Enter" && listItems[0].innerHTML !== ""){
-        addItem(listItems[0].lastChild.value)
+inputItem.addEventListener("keyup", (e) => {
+    if (e.key === "Enter" && inputItem.innerHTML !== ""){
+        addItem(inputText.value)
     }
 })
 
-
-
-function start() {
-    let li = document.createElement('li')
-    li.classList.add("list-item")
-    listItems.push(li)
-    li.innerHTML = `<input type="checkbox"><input type="text">`
-    result.appendChild(li);
-}
 
 function addItem(text){
     let li = document.createElement('li')
     li.classList.add("list-item")
     listItems.push(li)
     li.innerHTML = `<input type="checkbox"><p>${text}</p>`
-    console.log(result)
-    result.removeChild(result.childNodes.item(result.childNodes.length -1));
-    result.append(li)
-    start()
+    result.insertBefore(li, inputItem)
+    inputText.value = null
+}
+
+function deleteItems() {
+    [...result.children].forEach((item => {
+        if(item.firstChild.checked){
+            result.removeChild(item)
+        }
+    }))
+
 }
 
 
