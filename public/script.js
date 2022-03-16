3a03e6ade853f3055f5f8fc24fb0b6e0cf04ca71
sami <sami.sillanpaa@iki.fi>
2022-03-16 20:43:31 +0200

    Script addItem functionality started

--- public/script.js
+++ public/script.js
@@ -3,14 +3,13 @@ const buttons = document.querySelectorAll("button")
 const result = document.getElementById("result")
 const listItems = []
 
+start()
 
 buttons.forEach((button)=> {
     button.addEventListener("click", (e)=>{
         console.log(e.currentTarget.id)
         if(e.currentTarget.id === "lists-btn"){
             selectList()
-        } else if (e.currentTarget.id === "new-itm-btn") {
-            addItem()
         } else if (e.currentTarget.id === "delete-itm-btn") {
             deleteItem()
         } else if (e.currentTarget.id === "undo--btn") {
@@ -19,19 +18,28 @@ buttons.forEach((button)=> {
     })
 })
 
+listItems[0].addEventListener("keyup", (e) => {
+    if (e.key === "Enter" && listItems[0].innerHTML !== ""){
+        addItem(listItems[0].innerHTML)
+    }
+})
 
 
-function addItem() {
+
+function start() {
     const li = document.createElement('li')
     li.classList.add("list-item")
     listItems.push(li)
-    li.innerHTML = `
-            <input type="checkbox" name="check" id="check">
-            <input type="text">
-        `
-    console.log(listItems)
+    li.innerHTML = `<input type="checkbox"><input type="text">`
     result.appendChild(li);
 
 }
 
+function addItem(text){
+    const li = document.createElement('li')
+    li.classList.add("list-item")
+    listItems.push(li)
+    li.innerHTML = `<p>${text}</p>`
+    result.prepend(li);
+}
 
